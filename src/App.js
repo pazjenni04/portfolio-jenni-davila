a7d3e75652dd9a72a736cf5372a6f64419a0fae9
Jennifer Davila <pazjenni1331@gmail.com>
2021-09-22 17:17:32 -0500

    Added react package and bootstrap package.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+import React from "react";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Project from "./components/Project";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h2>Hello - this is your portfolio</h2>
+      {/* <Header />;
+      <Project />;
+      <Navigation />;
+      <Footer />; */}
+    </React.Fragment>
+  );
+}
+
+export default App;
