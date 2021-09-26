c100870eb3c706ae5f122a08e9ba8a80e4e6c7ec
Jennifer Davila <pazjenni1331@gmail.com>
2021-09-26 01:13:43 -0500

    updated header - still exploring react particles feat.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@ import React from "react";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import Project from "./components/Project";
-import backgroundSplash from "./images/background-floral.jpg";
 // import Footer from "./components/Footer";
-
 import "./App.css";
+
+//particles react package
+import { tsParticles } from "tsparticles";
 
 function App() {
   return (
     <React.Fragment>
-      <div
-        style={{
-          backgroundImage: `url(${backgroundSplash})`,
-          backgroundSize: "cover",
-        }}
-      >
-        <Navigation />;
-        <Header />
-        <Project />
-      </div>
+      <Navigation />;
+      <Header />
+      <Project />
     </React.Fragment>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Thailandpic from "../images/Thailand.png";
-import Hiking from "../images/Hiking.jpg";
-import Spain from "../images/Spain-fountain.jpg";
+import Typed from "react-typed";
 
 const style = {
   pFont: {
@@ -10,7 +8,8 @@ const style = {
   },
   headerTitle: {
     color: "#9A8AEC",
-    fontSize: "50px",
+    fontSize: "70px",
+    fontWeights: "10px",
   },
   blackBackground: {
     height: "100px",
@@ -21,34 +20,19 @@ const style = {
 
 function Header() {
   return (
-    <div className="container">
-      <div className="flex-row ">
-        <img
-          src={Thailandpic}
-          style={{
-            maxWidth: "300px",
-            marginRight: "20px",
-            marginLeft: "200px",
-          }}
-        />
-        <img src={Spain} style={{ maxWidth: "300px", marginRight: "20px" }} />
-        <img src={Hiking} style={{ maxWidth: "300px", marginRight: "20px" }} />
-      </div>
-      <div className="row align-items-center">
-        <div className="row">
-          <h2 style={style.headerTitle}>About Me</h2>
-        </div>
-        <div style={style.pFont}>
-          <p>Welcome to my portfolio!</p>
-          <p>
-            I'm an aspiring web developer with a background in marketing and
-            management. My current passion includes front-end coding and
-            creating a unique user experience. I live in Dallas, TX and am
-            currently undergoing the SMU coding bootcamp where I intend to
-            transition careers. My hobbies include traveling to far-away places
-            and becoming one with nature on my hiking adventures!
-          </p>
-        </div>
+    <div className="header-wrapper">
+      <div className="main-info">
+        <p style={style.headerTitle}>Welcome to my page!</p>
+        <Typed
+          className="typed-text"
+          strings={["HTML5", "CSS", "NODE.JS", "REACT.JS"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        ></Typed>
+        <a href="#" className="btn-main-offer">
+          Contact Me
+        </a>
       </div>
     </div>
   );
